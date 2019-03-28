da72d6b769a9af9f016e924a2a9713cf75ea13ef
Ruswelt <48466100+Ruswelt@users.noreply.github.com>
2019-03-28 12:23:55 +0530

    Update NewTest.java

--- src/test/java/sample/NewTest.java
+++ src/test/java/sample/NewTest.java
@@ -13,7 +13,7 @@ public class NewTest {
   @Test
   public void f() {
 	  
-	 System.setProperty("webdriver.gecko.driver", "./drivers/geckodriver");
+	 System.setProperty("webdriver.gecko.driver", "./drivers/geckodriver.exe");
 	 WebDriver driver = new FirefoxDriver();  
 	  driver.get("https://accounts.google.com/ServiceLogin/identifier?service=mail&passive=true&rm=false&continue=https%3A%2F%2Fmail.google.com%2Fmail%2F&ss=1&scc=1&ltmpl=default&ltmplcache=2&emr=1&osid=1&flowName=GlifWebSignIn&flowEntry=AddSession");
 	  WebElement gmail= driver.findElement(By.id("identifierId"));
