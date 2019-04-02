a862548911e7483c899165a4f1edfc67a955e602
Ruswelt <rusweltrus@gmail.com>
2019-04-02 11:53:24 +0530

    del

--- models/user.js
+++
@@ -1,9 +0,0 @@
-var mongoose = require('mongoose');
-var schema = mongoose.Schema;
-
-userShema = new schema({
-  name:{type:String,require:true},
-  email:{type:String,require:true}
-})
-
-module.exports = mongoose.model("User",userShema);
